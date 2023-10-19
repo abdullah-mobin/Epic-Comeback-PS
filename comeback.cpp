69d1f1f7c506239b7b7523fee77eea4c47630703
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-10-19 06:59:48 +0600

    hudai commit

---
+++ comeback.cpp
@@ -0,0 +1,23 @@
+//	Coder : Abdullah Mobin
+//	Created : 19/October/2023 06:58
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
+
+void soln()
+{
+    cout << "comeback" << endl;
+}
+int main()
+{
+    int __;
+    cin >> __ ;
+    while(__-- != 0) soln();
+
+    return 0;
+}
